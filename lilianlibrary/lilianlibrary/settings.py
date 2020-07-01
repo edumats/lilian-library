c643be205884eddc490ed64576fc8ee1f514e3f0
edumats <eduardo.mats@gmail.com>
2020-07-01 11:53:05 -0300

    changed ALLOWED_HOSTS to work in heroku

--- lilianlibrary/lilianlibrary/settings.py
+++ lilianlibrary/lilianlibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lilian-library.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
